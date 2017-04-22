3fdd3888e0a4c5bb8f1cf2cdfed9de6aa2c3ed79
骆晓啸 <xiao-xiao.luo@utsoft.cn>
2017-04-22 23:02:18 +0800

    spring整合mybatis时配置MapperScannerConfigurer 需将 <property name="sqlSessionFactory" ref="sqlSessionFactory"></property>
    改成<property name="sqlSessionFactoryBeanName" value="sqlSessionFactory"
    />

---
+++ spring-mybatis-dao/src/main/java/com/example/simples/sm/annotation/MybatisMapper.java
@@ -0,0 +1,5 @@
+package com.example.simples.sm.annotation;
+
+public @interface MybatisMapper {
+
+}
---
+++ spring-mybatis-dao/src/main/java/com/example/simples/sm/dao/UserMapper.java
@@ -0,0 +1,19 @@
+package com.example.simples.sm.dao;
+
+import com.example.simples.sm.annotation.MybatisMapper;
+import com.example.simples.sm.entity.User;
+
+@MybatisMapper
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
