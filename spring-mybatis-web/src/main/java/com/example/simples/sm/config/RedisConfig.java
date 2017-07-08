fcd1facce71e86c53647e82f485b8228a2ee5939
骆晓啸 <xiao-xiao.luo@utsoft.cn>
2017-07-08 12:28:13 +0800

    使用泛型 RedisTemplate<?, ?>

--- spring-mybatis-web/src/main/java/com/example/simples/sm/config/RedisConfig.java
+++ spring-mybatis-web/src/main/java/com/example/simples/sm/config/RedisConfig.java
@@ -25,12 +25,12 @@ import redis.clients.jedis.JedisPoolConfig;
  */
 //@Configuration
 public class RedisConfig {
-	
-//	private static final Logger logger=LoggerFactory.getLogger(RedisConfig.class);
-	
+
+	//	private static final Logger logger=LoggerFactory.getLogger(RedisConfig.class);
+
 	@Value("${redis.cluster.servers}")
 	private String servers;
-	
+
 	/**
 	 * redis 官方 cluster api
 	 * @return
@@ -38,15 +38,15 @@ public class RedisConfig {
 	@Bean
 	public JedisCluster jedisCluster(){
 		Set<HostAndPort> nodes=new HashSet<>();
-		for (String server : servers.split(",")) {
+		for (String server : this.servers.split(",")) {
 			String[] node=server.split(":");
 			nodes.add(new HostAndPort(node[0],Integer.parseInt(node[1])));
 		}
 		JedisCluster jedisCluster=new JedisCluster(nodes);
-		System.err.println(String.format("jedisCluster [%s] has been initialized...",servers));
+		System.err.println(String.format("jedisCluster [%s] has been initialized...",this.servers));
 		return jedisCluster;
 	}
-	
+
 	@Bean
 	public JedisPoolConfig jedisPoolConfig(){
 		JedisPoolConfig jedisPoolConfig=new JedisPoolConfig();
@@ -62,7 +62,7 @@ public class RedisConfig {
 		jedisPoolConfig.setNumTestsPerEvictionRun(-1);
 		return jedisPoolConfig;
 	}
-	
+
 	/**
 	 * require spring-data-redis 1.7+(require spring framework 4.2.8+)
 	 * spring 提供的redis cluster api
@@ -72,37 +72,37 @@ public class RedisConfig {
 	public RedisClusterConfiguration redisClusterConfiguration(){
 		RedisClusterConfiguration redisClusterConfiguration=new RedisClusterConfiguration();
 		Set<RedisNode> nodes=new HashSet<>();
-		for (String server : servers.split(",")) {
+		for (String server : this.servers.split(",")) {
 			String[] node=server.split(":");
 			nodes.add(new RedisNode(node[0],Integer.parseInt(node[1])));
 		}
 		redisClusterConfiguration.setClusterNodes(nodes);
 		return redisClusterConfiguration;
 	}
-	
+
 	@Bean
 	public RedisConnectionFactory jedisConnectionFactory() {
-		return new JedisConnectionFactory(redisClusterConfiguration());
-//		String[] hostInfo=servers.split(",")[0].split(":");
-//		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-//		jedisConnectionFactory.setPoolConfig(jedisPoolConfig());
-//		jedisConnectionFactory.setHostName(hostInfo[0]);
-//		jedisConnectionFactory.setPort(Integer.parseInt(hostInfo[1]));
-//		jedisConnectionFactory.setShardInfo(new JedisShardInfo("tcp://"+servers.split(",")[0])); // 适用于redis 2.x版本的分片模式
-//		return jedisConnectionFactory;
-//		 Redis Sentinel HA Cluster
-//		RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration() 
-//				.master("mymaster")
-//				.sentinel("127.0.0.1", 6380) 
-//				.sentinel("127.0.0.1", 6381)
-//				.sentinel("127.0.0.1", 6382);
-//		return new JedisConnectionFactory(sentinelConfig);
+		return new JedisConnectionFactory(this.redisClusterConfiguration());
+		//		String[] hostInfo=servers.split(",")[0].split(":");
+		//		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+		//		jedisConnectionFactory.setPoolConfig(jedisPoolConfig());
+		//		jedisConnectionFactory.setHostName(hostInfo[0]);
+		//		jedisConnectionFactory.setPort(Integer.parseInt(hostInfo[1]));
+		//		jedisConnectionFactory.setShardInfo(new JedisShardInfo("tcp://"+servers.split(",")[0])); // 适用于redis 2.x版本的分片模式
+		//		return jedisConnectionFactory;
+		//		 Redis Sentinel HA Cluster
+		//		RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+		//				.master("mymaster")
+		//				.sentinel("127.0.0.1", 6380)
+		//				.sentinel("127.0.0.1", 6381)
+		//				.sentinel("127.0.0.1", 6382);
+		//		return new JedisConnectionFactory(sentinelConfig);
 	}
 
 	@Bean
-	public RedisTemplate<Object, Object> redisTemplate() {
-		RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setConnectionFactory(jedisConnectionFactory());
+	public RedisTemplate<?, ?> redisTemplate() {
+		RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(this.jedisConnectionFactory());
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
 		return redisTemplate;
@@ -111,7 +111,7 @@ public class RedisConfig {
 	@Bean
 	public StringRedisTemplate stringRedisTemplate() {
 		StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-		stringRedisTemplate.setConnectionFactory(jedisConnectionFactory());
+		stringRedisTemplate.setConnectionFactory(this.jedisConnectionFactory());
 		stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
 		stringRedisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
 		return stringRedisTemplate;
